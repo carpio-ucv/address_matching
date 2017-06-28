1df1d6c112d8494a10ab161d544da11d11eb2656
carpio-ucv <carpio_ucv@yahoo.es>
2017-06-28 14:37:56 +0100

    Add files via upload
    Eliminating dashes and filters from blpu_(Improves accuracy).

--- matching_test_2015.R
+++ matching_test_2015.R
@@ -22,11 +22,12 @@ blpu_raw<-fread("BLPU.csv",  na.strings=c(""))
 
 # Creating blpu clean data set. # 78,595
 blpu_tidy<- blpu_raw %>% filter(LOGICAL_ST==1) %>%
-  mutate(filt=grepl("Residential",BLPU_CLA_1)) %>% # Filter Residential UPRN
-  filter(filt=="TRUE") %>%
-  select(UPRN, BLPU_CLA_1, MAP_X, MAP_Y, WARD_CODE) 
+ # mutate(filt=grepl("PP",BLPU_CLASS)) %>% # Filter Residential UPRN
+ # filter(filt=="FALSE") %>%
+  select(UPRN, BLPU_CLASS,BLPU_CLA_1, MAP_X, MAP_Y, WARD_CODE) #%>%
+  #filter(grepl("RH01|RH01|RH02|RH03|RI01|RI02|RI03",BLPU_CLASS))#NEW CODE
+  #filter(grepl("PP",BLPU_CLASS))#NEW CODE
 nrow(blpu_tidy)
-
 
 # Creating Gazeteer clean data set. # 78,841
 gazet_tidy<- gazet_raw %>% filter(LOGICAL_ST==1) %>% 
@@ -38,7 +39,8 @@ gazet_tidy<- gazet_raw %>% filter(LOGICAL_ST==1) %>%
   mutate(sfx1= ifelse(is.na(PAO_START1),"",PAO_START1)) %>% # Extract any suffix from PA
   mutate(sfx2= ifelse(is.na(SAO_START1),"",SAO_START1)) %>% # Extract any suffix from PA
   mutate(ID= paste0(No,sfx1,sfx2,POSTCODE)) %>% # create a unique identifier based on house number and postcode
-  mutate(ID=gsub(" ","",ID)) # remove empty spaces
+  mutate(ID=gsub(" ","",ID))%>% # remove empty spaces
+  mutate(ID=gsub("-","",ID))# remove dashes
 nrow(gazet_tidy)
 
 # Creating Final Gazeeter dataset # 74,852 (uno menos) - 74,553 Unique
@@ -46,7 +48,7 @@ gazet_df <- semi_join(gazet_tidy,blpu_tidy, by="UPRN") #%>%
   #distinct(ID) 
 nrow(gazet_df)
 
-# Creating GPs clean data set. #207,755 (uniques ID=67056)
+# Creating GPs clean data set. #207,755 (uniques ID=67056) # new unique
 gp_unique<- gp_raw %>% select(UPRN_match,FORENAME, SURNAME, PREMISES, STREET, POSTCODE) %>%
   mutate(PREMISES=replace(PREMISES,which(is.na(PREMISES)),""))%>%
   mutate(premises.no=gsub("[^0-9]","",PREMISES)) %>%  
@@ -54,7 +56,8 @@ gp_unique<- gp_raw %>% select(UPRN_match,FORENAME, SURNAME, PREMISES, STREET, PO
   mutate(str.no=ifelse(grepl("[0-99]",str.no)==TRUE,str.no,"")) %>%
   mutate(No= paste0(premises.no, str.no)) %>%
   mutate(ID= paste0(No, POSTCODE)) %>%
-  mutate(ID=gsub(" ","",ID)) %>%
+  mutate(ID=gsub(" ","",ID)) %>% # remove empty spaces
+  mutate(ID=gsub("-","",ID)) %>% # remove dashes
   distinct(ID) 
 nrow(gp_unique)
 
@@ -65,7 +68,8 @@ gp_tidy<- gp_raw %>% select(UPRN_match,FORENAME, SURNAME, PREMISES, STREET, POST
   mutate(str.no=ifelse(grepl("[0-99]",str.no)==TRUE,str.no,"")) %>%
   mutate(No= paste0(premises.no, str.no)) %>%
   mutate(ID= paste0(No, POSTCODE)) %>%
-  mutate(ID=gsub(" ","",ID))
+  mutate(ID=gsub(" ","",ID)) %>%
+mutate(ID=gsub("-","",ID)) # remove dashes
 nrow(gp_tidy)
 
 ##############################
@@ -75,33 +79,71 @@ nrow(gp_tidy)
 # Merging datasets of UNIQUE ID's
 #################################
 df<-semi_join(gp_unique,gazet_df, by="ID") 
-nrow(df) # 63,644
+nrow(df) # 64,195  new= 64,283
 no.match<-anti_join(gp_unique, gazet_df, by="ID")
-nrow(no.match)# 3,412
+nrow(no.match)# 2,818 new=2,730 
 # Accuracy
-paste0(round(100-nrow(no.match)/nrow(df)*100,2),"%")# "94.64%"
+paste0(round(100-nrow(no.match)/nrow(df)*100,3),"%")# 96.61% (new=95.753%)
 
 
 # Merging back datasets
 #################################
 df2<-inner_join(df, gazet_df, by="ID") 
-nrow(df2) # 63,784
+nrow(df2) # 64,464 new=64,562
+
 df_final<-inner_join(df2,gp_tidy, by="ID") 
-nrow(df_final) # 200,945
+nrow(df_final) # 203,710 new=204,145
 
 # Accuracy UPRN
 #################################
 
-audit<-df_final %>% mutate(check= ifelse(UPRN==UPRN_match,1,0))
+### Does not include Phil's non-matches
+audit<-df_final %>% filter(!is.na(UPRN_match)) %>%
+  filter(UPRN_match!="?") %>%
+  mutate(check= ifelse(UPRN==UPRN_match,1,0))
 
 perc<-audit %>% group_by(check) %>% count()  
-paste0(round(perc[2,2]/(perc[2,2]+perc[1,2])*100,3),"%") #98.895% | 2210 / 197813 
-                                             # previous: #98.799% | 2407 / 197934
+paste0(round(perc[2,2]/(perc[2,2]+perc[1,2])*100,3),"%") #98.374%  new="98.324%"
+
+wrongly.match<- audit %>% filter(check==0)
+
+# Accuracy TOTAL RECORS FROM GP'S
+#################################
+paste0(round(nrow(df_final) / nrow(gp_tidy),5)*100,"%") #98.053% new= 98.262%%
+
 # Visual inspection of no matches
 exp<-audit %>% filter(check==0) %>% select(PREMISES, STREET, ADDRESS)
 head(exp,200)
 exp2<-audit %>% filter(check==0) %>% select(UPRN, UPRN_match)
 head(exp2,200)
 
+
+# NO MATCHES
+names(gp_tidy)[1]<-"UPRN"
+gp.fil <- gp_tidy %>% filter(!is.na(UPRN)) %>% # filtering the ones no UPRN matched by Phil
+  filter(UPRN!="?")
+no<-anti_join(gp.fil, df_final, by="UPRN")
+nrow(no)# 4,224 new=3,942
+
+write.csv(no,"no_match.csv")
+write.csv(wrongly.match,"wrong_match.csv")
+
+# E.G.  110 CHELMER CRESCENT matched with parent shel by Phil (tHERE IS 110A, 110B)... 
+#       36 ESSEX ROAD       "" "" (IT HAS GROUND FLOOR AND FIRST FLOOR)
+#       177 Howard Road
+#       59 KING EDWARDS ROAD IG11 7TS (GAZEETER POST CODE FINISHES IN B).
+#       16 CROSSNESS ROAD- PEOPLE USED PREMISE NUMBER(16) BUT NOT PROPERTY NUMBER (22)
+
 ## CODES
 ## grepl("^[0-9].*$","hjg")
+
+gazet_df %>% filter(grepl("100057382",UPRN))#NEW CODE
+
+names(df_final)[14]<-"UPRN"
+
+filter(grepl("Faircross Care|Strathfield Gardens|Abbey Care|Westerley Lodge|
+              Chestnut Court Care|Keith Lauder House|Lisnaveane Lodge|Woodlands Rainham| 
+             Turning Point House| Parkview Nursing Home|Outlook Care|Chaseview Care Centre|
+             Sahara Parkside Apartments|Mayesbrook Home For The Aged|Cloud House|Outlook 
+             Care|Lisaveane House|Gascogine Road Care Home|Bennetts Castle Care Home|
+             Kallar Lodge|Cherry Orchard|Richard Ryan Place|Hanbury Court",ADDRESS)) #NEW CODE
